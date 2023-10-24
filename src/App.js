ea23a8e944c76b42778c4c4bd2b5394677d6b6fd
Illia Ovhcaruk <ilaovcaruk0@gmail.com>
2023-10-24 11:08:50 +0100

    problem with fav

--- src/App.js
+++ src/App.js
@@ -1,129 +1,113 @@
-import axios from "axios";
-import React from "react";
-import Header from "./components/Header";
-import Rightsidebar from "./components/Rightsidebar";
-import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
-import Favorites from "./pages/Favorites";
+import axios from 'axios'
+import React from 'react'
+import Header from './components/Header'
+import Rightsidebar from './components/Rightsidebar'
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import Favorites from './pages/Favorites'
 
-const AppContext = React.createContext({});
-//  console.log(AppContext);
+export const AppContext = React.createContext({})
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [cartItems, setCartItems] = React.useState([]);
-  const [favorites, setFavorites] = React.useState([]);
-  const [cartOpened, setCartOpened] = React.useState(false);
-  const [searchValue, setSearchValue] = React.useState("");
+	const [items, setItems] = React.useState([])
+	const [cartItems, setCartItems] = React.useState([])
+	const [favorites, setFavorites] = React.useState([])
+	const [cartOpened, setCartOpened] = React.useState(false)
+	const [searchValue, setSearchValue] = React.useState('')
 
-  React.useEffect(() => {
-    async function fetchData() {
-      try {
-        const itemsResponse = await axios.get(
-          "https://650f113054d18aabfe99cccb.mockapi.io/items"
-        );
-        const cartResponse = await axios.get(
-          "https://650f113054d18aabfe99cccb.mockapi.io/cart"
-        );
-        const favoritesResponse = await axios.get(
-          "https://650f113054d18aabfe99cccb.mockapi.io/favorites"
-        );
+	React.useEffect(() => {
+		async function fetchData() {
+			try {
+				const itemsResponse = await axios.get(
+					'https://650f113054d18aabfe99cccb.mockapi.io/items',
+				)
+				const cartResponse = await axios.get(
+					'https://650f113054d18aabfe99cccb.mockapi.io/cart',
+				)
+				const favoritesResponse = await axios.get(
+					'https://650f113054d18aabfe99cccb.mockapi.io/favorites',
+				)
 
-        // Встановлюємо дані з відповідей Axios до відповідних станів
-        setItems(itemsResponse.data);
-        setCartItems(cartResponse.data);
-        setFavorites(favoritesResponse.data);
-      } catch (error) {
-        // Обробка помилок, якщо є
-      }
-    }
-    fetchData();
-  }, []);
+				setItems(itemsResponse.data)
+				setCartItems(cartResponse.data)
+				setFavorites(favoritesResponse.data)
+			} catch (error) {}
+		}
+		fetchData()
+	}, [])
 
-  const onAddToCart = (obj) => {
-    if (cartItems.find((item) => Number(item.id) === Number(obj.id))) {
-      axios.delete(
-        `https://650f113054d18aabfe99cccb.mockapi.io/cart/${obj.id}`
-      );
-      setCartItems((prev) =>
-        prev.filter((item) => Number(item.id) !== Number(obj.id))
-      );
-    } else {
-      axios.post("https://650f113054d18aabfe99cccb.mockapi.io/cart", obj);
-      setCartItems((prev) => [...prev, obj]);
-    }
-  };
+	const onAddToCart = obj => {
+		if (cartItems.find(item => Number(item.id) === Number(obj.id))) {
+			axios.delete(`https://650f113054d18aabfe99cccb.mockapi.io/cart/${obj.id}`)
+			setCartItems(prev => prev.filter(item => Number(item.id) !== Number(obj.id)))
+		} else {
+			axios.post('https://650f113054d18aabfe99cccb.mockapi.io/cart', obj)
+			setCartItems(prev => [...prev, obj])
+		}
+	}
 
-  const onRemoveItem = (id) => {
-    axios.delete(`https://650f113054d18aabfe99cccb.mockapi.io/cart/${id}`);
-    setCartItems((prev) => prev.filter((item) => item.id !== id));
-  };
+	const onRemoveItem = id => {
+		axios.delete(`https://650f113054d18aabfe99cccb.mockapi.io/cart/${id}`)
+		setCartItems(prev => prev.filter(item => item.id !== id))
+	}
 
-  const onAddToFavourite = async (obj) => {
-    console.log(obj);
-    try {
-      //try catch что бы отловить ошибку которая может произойти при вызове запроса
-      if (favorites.find((obj) => obj.id === obj.id)) {
-        axios.delete(
-          `https://650f113054d18aabfe99cccb.mockapi.io/favorites/${obj.id}`
-        );
-      } else {
-        const { data } = await axios.post(
-          "https://650f113054d18aabfe99cccb.mockapi.io/favorites",
-          obj
-        ); //берем обьект который выдаст с  бекенд а не с основной страницы
-        setFavorites((prev) => [...prev, data]); //если await не отрабатывает покажи нам это через алерт
-      }
-    } catch (error) {
-      alert("Не удалось добавить в фавориты ");
-    }
-  };
+	const onAddToFavorite = async obj => {
+		console.log(obj)
+		try {
+			if (favorites.find(obj => obj.id === obj.id)) {
+				axios.delete(`https://650f113054d18aabfe99cccb.mockapi.io/favorites/${obj.id}`)
+			} else {
+				const { data } = await axios.post(
+					'https://650f113054d18aabfe99cccb.mockapi.io/favorites',
+					obj,
+				)
+				setFavorites(prev => [...prev, data])
+			}
+		} catch (error) {
+			alert('Не удалось добавить в фавориты ')
+		}
+	}
 
-  const onChangeSearchInput = (e) => {
-    setSearchValue(e.target.value);
-  };
+	const onChangeSearchInput = e => {
+		setSearchValue(e.target.value)
+	}
 
-  return (
-    <AppContext.Provider value={{ items, cartItems, favorites }}>
-      <div className="wrapper">
-        {cartOpened ? (
-          <Rightsidebar
-            items={cartItems}
-            onClose={() => setCartOpened(false)}
-            onRemove={onRemoveItem}
-          />
-        ) : null}
+	return (
+		<AppContext.Provider value={{ items, cartItems, favorites }}>
+			<div className='wrapper'>
+				{cartOpened ? (
+					<Rightsidebar
+						items={cartItems}
+						onClose={() => setCartOpened(false)}
+						onRemove={onRemoveItem}
+					/>
+				) : null}
 
-        <Header onClickCart={() => setCartOpened(true)} />
+				<Header onClickCart={() => setCartOpened(true)} />
 
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home
-                items={items}
-                cartItems={cartItems}
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-                onChangeSearchInput={onChangeSearchInput}
-                onAddToFavourite={onAddToFavourite}
-                onAddToCart={onAddToCart}
-              />
-            }
-          />
-          <Route
-            path="/favorites"
-            element={
-              <Favorites
-                favorites={favorites}
-                onAddToFavourite={onAddToFavourite}
-              />
-            }
-          />
-        </Routes>
-      </div>
-    </AppContext.Provider>
-  );
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<Home
+								items={items}
+								cartItems={cartItems}
+								searchValue={searchValue}
+								setSearchValue={setSearchValue}
+								onChangeSearchInput={onChangeSearchInput}
+								onAddToFavorite={onAddToFavorite}
+								onAddToCart={onAddToCart}
+							/>
+						}
+					/>
+					<Route
+						path='/favorites'
+						element={<Favorites onAddToFavorite={onAddToFavorite} />}
+					/>
+				</Routes>
+			</div>
+		</AppContext.Provider>
+	)
 }
 
-export default App;
+export default App
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,33 +1,29 @@
-import React from "react";
-import Card from "../components/Card";
-import AppContext from "../App";
+import React, { useContext } from 'react'
+import Card from '../components/Card'
+import { AppContext } from '../App'
 
-function Favorites({ onAddToFavourite }) {
-  const state = React.useContext(AppContext);
-  console.log(state);
+function Favorites({ onAddToFavorite }) {
+	const state = useContext(AppContext)
+	// console.log(AppContext)
+	console.log (state)
 
-  return (
-    <div className="wrapperMakr">
-      <div className="titleMark">
-        <h1>Мои закладки</h1>
-      </div>
-      <div className="content-Mark">
-        {[].map(
-          (
-            item,
-            index //пробегаемся по масиву и скавим уникальный ключ в виде index
-          ) => (
-            <Card
-              key={index.id}
-              favourited={true}
-              onFavourite={onAddToFavourite}
-              {...item} //конкретенация строк  title,price,imageUrl (возьми все от)
-            />
-          )
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className='wrapperMakr'>
+			<div className='titleMark'>
+				<h1>Мои закладки</h1>
+			</div>
+			<div className='content-Mark'>
+				{[].map((item, index) => (
+					<Card
+						key={index.id}
+						favourited={true}
+						onFavorite={onAddToFavorite}
+						{...item}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
 
-export default Favorites;
+export default Favorites
